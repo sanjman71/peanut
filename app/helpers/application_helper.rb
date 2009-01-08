2f048674a115823160f1a63420058a73c1909a1c
Sanjay <sanjay@jarna.com>
2009-01-08 14:27:26 -0600

    added yield :title for setting page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
+  def title(page_title)
+    content_for(:title)  { page_title }
+  end
+  
   def javascript(*files)
     content_for(:javascript) { javascript_include_tag(*files) }
   end
