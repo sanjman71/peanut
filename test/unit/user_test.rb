f6d5a82aaacbc37402d5633e1549ba745769b781
Sanjay <sanjay@jarna.com>
2009-01-27 07:13:14 -0600

    fixed user unit test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,10 +3,9 @@ require 'test/factories'
 
 class UserTest < ActiveSupport::TestCase
 
-  context "create with 0 invitation" do
+  context "create without an invitation" do
     setup do
-      @user1 = User.create(:company_id => 0, :email => "user1@jarna.com", :password => "secret", :password_confirmation => "secret",
-                           :invitation_id => 0)
+      @user1 = User.create(:company_id => 0, :email => "user1@jarna.com", :password => "secret", :password_confirmation => "secret")
     end
     
     should "be a valid user" do
@@ -14,10 +13,9 @@ class UserTest < ActiveSupport::TestCase
     end
     should_change "User.count", :by => 1
     
-    context "create another user with 0 invitation" do
+    context "create another user without an invitation" do
       setup do
-        @user2 = User.create(:company_id => 0, :email => "user2@jarna.com", :password => "secret", :password_confirmation => "secret",
-                             :invitation_id => 0)
+        @user2 = User.create(:company_id => 0, :email => "user2@jarna.com", :password => "secret", :password_confirmation => "secret")
       end
       
       should "be a valid user" do
@@ -26,16 +24,17 @@ class UserTest < ActiveSupport::TestCase
       should_change "User.count", :by => 1
     end
 
-    context "create another user with empt invitation" do
-      setup do
-        @user3 = User.create(:company_id => 0, :email => "user2@jarna.com", :password => "secret", :password_confirmation => "secret")
-      end
-      
-      should "not be a valid user" do
-        assert !@user3.valid?
-        assert_match /blank/, @user3.errors[:invitation_id]
-      end
-      should_not_change "User.count"
-    end
+    # context "create user with a valid invitation" do
+    #   setup do
+    #     @invitation1 = Invitation.create
+    #     @user3       = User.create(:company_id => 0, :email => "user2@jarna.com", :password => "secret", :password_confirmation => "secret")
+    #   end
+    #   
+    #   should "not be a valid user" do
+    #     assert !@user3.valid?
+    #     assert_match /blank/, @user3.errors[:invitation_id]
+    #   end
+    #   should_not_change "User.count"
+    # end
   end
 end
