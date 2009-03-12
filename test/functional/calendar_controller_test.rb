0a2194b48493604f4f3297b9445ae4431c279e12
Sanjay <sanjay@jarna.com>
2009-03-12 18:19:45 -0500

    added calendar format test case

--- test/functional/calendar_controller_test.rb
+++ test/functional/calendar_controller_test.rb
@@ -5,6 +5,8 @@ class CalendarControllerTest < ActionController::TestCase
 
   # show calendar for a specific schedulable
   should_route :get, 'users/1/calendar',  :controller => 'calendar', :action => 'show', :schedulable_type => 'users', :schedulable_id => 1
+  should_route :get, 'users/1/calendar.pdf',  
+               :controller => 'calendar', :action => 'show', :schedulable_type => 'users', :schedulable_id => 1, :format => 'pdf'
   
   # search calendar for a specific schedulable
   should_route :post, 'users/1/calendar/search', 
