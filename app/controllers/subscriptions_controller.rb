b19e1271a7b0a554d51eb9683b4cdee0eb3c7c8c
Sanjay <sanjay@jarna.com>
2009-04-02 21:31:49 -0500

    removed subscription index action

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,23 +1,7 @@
 class SubscriptionsController < ApplicationController
   layout "company"
 
-  privilege_required 'update companies', :only => [:index, :edit, :update], :on => :current_company
-  
-  # GET /subscriptions
-  def index
-    case params[:filter]
-    when 'errors'
-      @subscriptions  = Subscription.billing_errors
-      @search_text    = "Subscriptions w/ Billing Errors (#{@subscriptions.size})" 
-    else
-      @subscriptions  = Subscription.all
-      @search_text    = "All Subscriptions (#{@subscriptions.size})" 
-    end
-    
-    respond_to do |format|
-      format.html { render(:action => :index, :layout => 'home')}
-    end
-  end
+  privilege_required 'update companies', :only => [:edit, :update], :on => :current_company
   
   def edit
     @subscription = current_company.subscription
