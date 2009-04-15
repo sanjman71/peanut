c05b032d5a7c1fb07325e828d273c2ca811ca2ae
Sanjay <sanjay@jarna.com>
2009-04-14 20:55:32 -0500

    removed plans controller since its not been used

--- test/functional/plans_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test/test_helper'
-require 'test/factories'
-
-class PlansControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
