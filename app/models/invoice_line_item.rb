584a3be3ccbfa087761ede72f1d09a57a6eb05d8
Killian Murphy <killian@killianmurphy.com>
2009-07-13 15:35:55 -0500

    moved all movels to the walnut app, will be using app/models

--- app/models/invoice_line_item.rb
+++
@@ -1,5 +0,0 @@
-class InvoiceLineItem < ActiveRecord::Base
-  belongs_to                :chargeable, :polymorphic => true
-  belongs_to                :invoice
-  validates_presence_of     :invoice_id, :price_in_cents, :chargeable_type, :chargeable_id
-end
--- app/models/notes_subject.rb
+++
@@ -1,5 +0,0 @@
-class NotesSubject < ActiveRecord::Base
-  belongs_to                :note
-  belongs_to                :subject, :polymorphic => true
-  validates_presence_of     :note_id, :subject_id, :subject_type
-end
