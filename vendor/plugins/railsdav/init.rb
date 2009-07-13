63279344547e7072dd05420bf4ae157877d0902f
Killian Murphy <killian@killianmurphy.com>
2009-07-13 15:07:30 -0500

    removed file-based webdav features from railsdav for now

--- vendor/plugins/railsdav/init.rb
+++ vendor/plugins/railsdav/init.rb
@@ -1,4 +1,4 @@
 require 'railsdav'
 
 ActionController::Base.send(:include, Railsdav)
-ActionController::Base.send(:include, Railsdav::ActAsFileWebDav)
+# ActionController::Base.send(:include, Railsdav::ActAsFileWebDav)
--- vendor/plugins/railsdav/lib/railsdav.rb
+++ vendor/plugins/railsdav/lib/railsdav.rb
@@ -12,7 +12,7 @@ require 'railsdav/resource'
 require 'railsdav/propxml'
 require 'railsdav/act_as_railsdav'
 
-
-require 'webdav/act_as_filewebdav'
-require 'webdav/file_resource'
+# 
+# require 'webdav/act_as_filewebdav'
+# require 'webdav/file_resource'
 
