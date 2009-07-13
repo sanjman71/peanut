c4ab5802d483443ce56d8fc68d71a880521e6f28
Killian Murphy <killian@killianmurphy.com>
2009-07-13 15:35:55 -0500

    moved all movels to the walnut app, will be using app/models

--- app/models/company_service.rb
+++
@@ -1,4 +0,0 @@
-class CompanyService < ActiveRecord::Base
-  belongs_to                :company  # the counter cache is managed using callbacks in the company model
-  belongs_to                :service
-end
