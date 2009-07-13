f15e123c529c10594d917c4d8099416b48804b8e
Killian Murphy <killian@killianmurphy.com>
2009-07-12 19:15:32 -0500

    removed shared models directory. This directory is now in app/walnut_shared_models, and must be symbolically linked to the walnut application

--- walnut_shared_models/location_neighbor_collection.rb
+++
@@ -1,9 +0,0 @@
-class LocationNeighborCollection < ::Array
-  
-  def each_with_geodist(&block)
-    self.each_with_index do |location_neighbor, index|
-      yield location_neighbor.neighbor, location_neighbor.distance
-    end
-  end
-
-end
--- walnut_shared_models/township.rb
+++
@@ -1,8 +0,0 @@
-class Township
-  
-  # import all townships
-  def self.import
-    City.import(:file => "#{RAILS_ROOT}/data/townships.txt")
-  end
-  
-end
