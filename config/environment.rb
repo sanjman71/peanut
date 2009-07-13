2c666b5c79b7983ca52adb72cdcd49a1773fc5ab
Sanjay <sanjay@jarna.com>
2009-07-12 19:25:23 -0500

    removed has_many_polymorphs gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -33,7 +33,6 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.6', :lib => 'will_paginate', :source => "http://gems.github.com"
   config.gem 'sanitize', :version => '~> 1.0.6', :source => "http://gems.github.com"
   config.gem 'prawn', :version => '~> 0.4.1'
-  config.gem 'has_many_polymorphs', :version => '~> 2.13'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
