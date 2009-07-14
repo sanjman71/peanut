313128e31c6adbcbc4f6f417332afe640f361a3a
Sanjay <sanjay@jarna.com>
2009-07-13 20:51:44 -0500

    fixed rpx action

--- app/controllers/rpx_controller.rb
+++ app/controllers/rpx_controller.rb
@@ -2,21 +2,18 @@ class RpxController < ApplicationController
 
   def customer
     raise Exception unless @data = RPXNow.user_data(params[:token])
-    # raise Exception, "id: #{data[:identifier]}, name: #{data[:name]}, email: #{data[:email]}, display: #{data[:username]}, #{data.keys}"
     
     @user = User.find_by_identifier(@data[:identifier])
     
     if @user.blank?
       # create user using rpx identifier
-      # @user = User.new(:name => @data[:name], :email => @data[:email], :identity => @data[:identifier])
-      @user = User.new(:name => @data[:name], :email => @data[:email], :identifier => @data[:identifier], 
-                       :password => "secret", :password_confirmation => "secret")
+      @user = User.create(:name => @data[:name], :email => @data[:email], :identifier => @data[:identifier])
     
       @user.register! if @user && @user.valid?
       success = @user && @user.valid?
     
       if success && @user.errors.empty?
-        @redirect_path  = "/" #return_to || "/"
+        @redirect_path  = "/"
       else
         raise Exception, "error"
       end
