4dc4828942d96dd2fab3eef721a9640549d7386c
Killian Murphy <killian@killianmurphy.com>
2009-07-15 21:33:02 -0500

    cal_dav_resource_test initial version. Not enough, but started

---
+++ test/unit/cal_dav_resource_test.rb
@@ -0,0 +1,33 @@
+require 'test/test_helper'
+require 'test/factories'
+
+class CalDavResourceTest < ActiveSupport::TestCase
+  
+  def setup
+    @owner          = Factory(:user, :name => "Owner")
+    @monthly_plan   = Factory(:monthly_plan)
+    @subscription   = Subscription.new(:user => @owner, :plan => @monthly_plan)
+    @company        = Factory(:company, :subscription => @subscription)
+    @anywhere       = Location.anywhere
+  end
+      
+  context "create free time" do
+    setup do
+      # create free time from 10 am to 12 pm
+      @free_service     = @company.free_service
+      @johnny           = Factory(:user, :name => "Johnny", :companies => [@company])
+      @today            = Time.now.to_s(:appt_schedule_day) # e.g. 20081201
+      @time_range       = TimeRange.new({:day => @today, :start_at => "1000", :end_at => "1200"})
+      @free_appt        = AppointmentScheduler.create_free_appointment(@company, @johnny, @free_service, :time_range => @time_range)
+      @cal_dav_resource = CalDavResource.new(@company.appointments, @company)      
+    end
+
+    should_change "Appointment.count", :by => 1
+
+    should "give icalendar" do
+      assert_not_nil @cal_dav_resource.data
+    end
+    
+  end    
+
+end
