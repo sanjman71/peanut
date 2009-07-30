d6fd50aa8e0777ce42db72bac56aafe03712ef88
Sanjay <sanjay@jarna.com>
2009-07-30 17:33:02 -0500

    changed weekly schedule page to use jquery timepickr

--- public/javascripts/calendar_weekly.js
+++ public/javascripts/calendar_weekly.js
@@ -6,13 +6,26 @@ $.fn.set_weekly_schedule = function () {
   $('.dayname.mark').each(function() {
     daynames.push($(this).attr("id"));
   })
-  
-  if (daynames.length > 0) {
-    s = daynames.join(", ");
+
+  if (daynames.length == 0) {
+    // pick a day before a weekly schedule can be set
+    $("#weekly_schedule").text(s);
+    return;
   }
   
+  s = daynames.join(", ");
+
+  // find start time
+  if ($("#starts_at").attr('value')) {
+    s += " starting at " + $("#starts_at").attr('value');
+
+    // check end time
+    var tend = $("#ends_at").attr('value');
+    if (tend) { s += " and ending at " + tend; }
+  }
+
   // find slider start time
-  if ($("#slider_start").find(".time.mark:first").length) {
+/*  if ($("#slider_start").find(".time.mark:first").length) {
     var tstart = $("#slider_start").find(".time.mark:first").attr('id');
     s += " starting at " + tstart;
 
@@ -20,7 +33,7 @@ $.fn.set_weekly_schedule = function () {
     var tend = $("#slider_end").find(".time.mark:first").attr('id');
     if (tend) { s += " and ending at " + tend; }
   }
-  
+*/  
   $("#weekly_schedule").text(s);
 }
 
@@ -38,21 +51,22 @@ $.fn.set_recurrence = function () {
   }
   
   // write recurrence 'tstart' and 'tend', append '00' for seconds
-  if ($("#slider_start").find(".time.mark:first").length) {
-    var tstart = $("#slider_start").find(".time.mark:first").attr("id") + '00';
-    $("#tstart").attr('value', tstart);
-  }
-  
-  if ($("#slider_end").find(".time.mark:first").length) {
-    var tend = $("#slider_end").find(".time.mark:first").attr("id") + '00';
-    $("#tend").attr('value', tend);
+  if ($("#starts_at").attr('value')) {
+    // format 'start_at' field for 'dstart'
+    s = $(document).convert_time_ampm_to_string($("#starts_at").attr('value'));
+    $("#tstart").attr('value', s);
   }
 
+  if ($("#ends_at").attr('value')) {
+    // format 'start_at' field for 'dstart'
+    s = $(document).convert_time_ampm_to_string($("#ends_at").attr('value'));
+    $("#tend").attr('value', s);
+  }
+  
   // write recurrence 'dstart'
   if ($("#schedule_start_date").attr('value')) {
-    date   = Date.fromString($("#schedule_start_date").attr('value'));
-    dstart = date.asString("yyyymmdd");
-    $("#dstart").attr('value', dstart);
+    s = $(document).convert_date_to_string($("#schedule_start_date").attr('value'));
+    $("#dstart").attr('value', s);
   } else {
     // clear out field
     $("#dstart").attr('value', '');
@@ -60,25 +74,15 @@ $.fn.set_recurrence = function () {
 
   // write recurrence 'until'
   if ($("#schedule_end_date").attr('value')) {
-    date = Date.fromString($("#schedule_end_date").attr('value'));
-    dend = date.asString("yyyymmdd");
-    $("#until").attr('value', dend);
+    s = $(document).convert_date_to_string($("#schedule_end_date").attr('value'));
+    $("#until").attr('value', s);
   } else {
     // clear out field
     $("#until").attr('value', '');
   }
 }
 
-$(document).ready(function() {
-
-  // initialize datepicker
-  Date.firstDayOfWeek = 7;
-  Date.format = 'mm/dd/yyyy';
-  $(document).init_datepicker({start_date : (new Date()).addDays(1).asString(), end_date : (new Date()).addMonths(3).asString(), max_days:10});
-
-  // initialize weekly schedule
-  $(document).set_weekly_schedule();
-
+$.fn.init_daynames = function () {
   // set hover states to show selected day name
   $(".dayname").hover(
     function() {
@@ -96,13 +100,9 @@ $(document).ready(function() {
     $(document).set_weekly_schedule();
     $(document).set_recurrence();
   });
-  
-  // show start, end time as they are selected
-  $(".slider .time").bind("afterClick", function () {
-    $(document).set_weekly_schedule();
-    $(document).set_recurrence();
-  })
+}
 
+$.fn.init_schedule_range = function () {
   $("#schedule_end_until").click(function () {
     // show date field
     $("#schedule_end_date").show();
@@ -117,18 +117,30 @@ $(document).ready(function() {
     $(document).set_weekly_schedule();
     $(document).set_recurrence();
   })
+}
 
-  // bind to start and end date selected events
-  $('#schedule_end_date').bind(
-    'dpClosed',
-    function(e, selectedDates)
-    {
-      // update recurrence
-      $(document).set_weekly_schedule();
-      $(document).set_recurrence();
-    }
-  )
-  
+$.fn.init_datepicker = function () {
+  $(".datepicker").datepicker({
+      minDate: +0, 
+      maxDate: '+3m',
+      onClose: function(dateText, instance) {
+        $(document).set_weekly_schedule();
+        $(document).set_recurrence();
+      }
+    });
+}
+
+$.fn.init_timepicker = function() {
+  $(".timepicker").timepickr({
+      convention:12, 
+      select: function() { 
+        $(document).set_weekly_schedule();
+        $(document).set_recurrence();
+      }
+    });
+}
+
+$.fn.init_schedule_form = function () {
   // submit weekly schedule
   $("#add_weekly_schedule_form").submit(function () {
     // validate presence of byday, tstart, tend, schedule_start_date
@@ -152,8 +164,49 @@ $(document).ready(function() {
       return false
     }
 
-    alert("values passed");
-    
-    return false;
+    return true;
   })
+}
+
+// convert mm/dd/yyyy date to yyyymmdd string
+$.fn.convert_date_to_string = function(s) {
+  re    = /(\d{2,2})\/(\d{2,2})\/(\d{4,4})/
+  match = s.match(re);
+  if (!match) {
+    s = ''
+  } else {
+    s = match[3] + match[1] + match[2]
+  }
+  return s
+}
+
+// convert '03:00 pm' time format to 'hhmmss' 24 hour time format
+$.fn.convert_time_ampm_to_string = function(s) {
+  re      = /(\d{2,2}):(\d{2,2}) (am|pm)/
+  match   = s.match(re);
+
+  // convert hour to integer, leave minute as string
+  hour    = parseInt(match[1], 10); 
+  minute  = match[2];
+
+  if (match[3] == 'pm') {
+    // add 12 for pm
+    hour += 12;
+  }
+
+  value = hour < 10 ? "0" + hour.toString() : hour.toString()
+  value += minute + "00";
+  return value
+}
+
+$(document).ready(function() {
+
+  $(document).init_daynames();
+  $(document).init_schedule_range();
+  $(document).init_datepicker();
+  $(document).init_timepicker();
+  $(document).init_schedule_form();
+
+  // initialize weekly schedule
+  $(document).set_weekly_schedule();
 })
