69bfc4aafbca6a571c2b26b8ccd29aca2928e2ee
Sanjay <sanjay@jarna.com>
2009-08-14 18:49:11 -0500

    added jquery tab test interface

---
+++ public/javascripts/home.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#header-tabs").tabs({selected: -1, ajaxOptions: { async: null, cache: true }})
+
+  $("#header-tabs").bind('tabsselect', function(event, ui) {
+    // simple implentation that uses static array of tab elements
+    site_tabs = ['companies', 'signup']
+    site_href = location.href.split("/")[3];
+    
+    // find all tab elements
+    $("li.ui-state-default").each(function(i) {
+      if (site_tabs[i] == site_href) {
+        $(this).addClass('ui-state-active');
+      }
+    })
+
+    var url = $.data(ui.tab, 'load.tabs');
+    console.debug("tab url: " + url);
+
+    return false;
+  });
+
+  // select the first tab
+  console.debug("selecting tab");
+  $('#header-tabs').tabs('select', 0);
+  console.debug("selected: " + $('#header-tabs').tabs('option', 'selected'));
+})
