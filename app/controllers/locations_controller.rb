4f394ecf0e91bc7c85ac9dadb4c5d36aec4d0af2
Killian Murphy <killian@killianmurphy.com>
2009-08-27 17:55:17 -0700

    interim work on locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -62,10 +62,6 @@ class LocationsController < ApplicationController
   # GET /locations/1/edit
   def edit
     @location = Location.find(params[:id])
-    @location.city_str = @location.city.name
-    @location.state_str = @location.state.code
-    @location.zip_str = @location.zip.name
-    @location.country_str = @location.country.code
     
     respond_to do |format|
       format.html # edit.html.haml
@@ -77,7 +73,6 @@ class LocationsController < ApplicationController
   # POST /locations
   # POST /locations.xml
   def create
-    
     if !current_company
       flash[:error] = "To add a location you must be working with a specific company."
       redirect_to root_path and return
@@ -87,10 +82,12 @@ class LocationsController < ApplicationController
       flash[:error] = "Your plan does not allow you to add another location."
       redirect_to(edit_company_root_path(:subdomain => current_subdomain)) and return
     end
+        
+    @location = initialize_location(Location.new, params[:location])
     
-    @location = Location.new(params[:location])
+    @location.update_attributes(params[:location]) unless !@location.errors.empty?
 
-    if current_company.locations << @location 
+    if @location.errors.empty? && current_company.locations << @location
     
       flash[:notice] = "Location was successfully added to #{current_company.name}"
 
@@ -111,14 +108,18 @@ class LocationsController < ApplicationController
   def update
     @location = Location.find(params[:id])
 
-    if @location.update_attributes(params[:location])
+    @location = initialize_location(@location, params[:location])
+
+    @location.update_attributes(params[:location]) unless !@location.errors.empty?
+
+    if @location.errors.empty?
       respond_to do |format|
         flash[:notice] = 'Location was successfully updated.'
         format.html { redirect_to(redirect_success_path) }
       end
     else
       respond_to do |format|
-        flash[:notice] = 'Problem updating location.'
+        flash[:error] = 'Problem updating location.'
         format.html { render :action => "edit" }
       end
     end
@@ -148,6 +149,71 @@ class LocationsController < ApplicationController
 
   def redirect_success_path
     edit_company_root_path(:subdomain => current_subdomain)
+  end
+  
+  #
+  # initialize_location is set up to initialize the location in the correct order
+  # This means that we want to make sure to pick the correct city id depending on the state
+  # For example, if there's a Springfield, IL and Springfield, CA, we want to pick the correct one
+  # depending on which state is chosen
+  #
+  def initialize_location(location, params)
+
+    # Initialize the country, state, city and zip in order
+    if params[:country_id]
+      new_country = Country.find(params[:country_id].to_i) unless location.country_id == params[:country_id].to_i
+      if new_country
+        location.country = new_country
+      elsif location.country_id != params[:country_id].to_i
+        location.errors.add(:country, "is missing or invalid")
+      end
+    else
+      location.errors.add(:country, "is missing or invalid")      
+    end
+    
+    if params[:state_id]
+      new_state = State.find(params[:state_id].to_i) unless location.state_id == params[:state_id].to_i
+      if new_state
+        location.state = new_state
+      elsif location.state_id != params[:state_id].to_i
+        location.errors.add(:state, "is missing or invalid")
+      end
+    else
+      location.errors.add(:state, "is missing or invalid")
+    end
+    
+    if !params[:city].blank? && location.state
+      location.city = location.state.cities.find_or_create_by_name(params[:city])
+    else
+      if !location.state
+        location.errors.add(:city, "can't be added without state")
+      else
+        location.errors.add(:city, "cannot be blank")
+      end
+    end
+    
+    if !params[:zip].blank? && location.state
+      location.zip = location.state.zips.find_or_create_by_name(params[:zip])
+    else
+      if !location.state
+        location.errors.add(:zip, "can't be added without state")
+      else
+        location.errors.add(:zip, "cannot be blank")
+      end
+    end
+
+    # Catch a street address error if appropriate
+    if params[:street_address].blank?
+      location.errors.add(:street_address, "can't be blank")
+    end
+
+    # Remove these parameters - we don't want to pass them to update_attributes or create
+    params.delete(:country_id)
+    params.delete(:state_id)
+    params.delete(:city)
+    params.delete(:zip)
+    
+    location
   end
   
 end
