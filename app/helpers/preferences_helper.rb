58b62093dbfbe71417cb7d52c70e2f666a163ac5
Killian Murphy <killian@killianmurphy.com>
2009-09-09 23:46:04 -0700

    updated preferences list to use proper values for # days etc.

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -1,13 +1,13 @@
 module PreferencesHelper
   def time_horizon_options
     [
-      ["1 week", 7],
-      ["2 weeks", 14],
-      ["4 weeks", 28],
-      ["60 days (approx. 2 months)", 60],
-      ["90 days (approx. 3 months)", 90],
-      ["180 days (approx. 6 months)", 180],
-      ["365 days (approx. 1 year)", 365]
+      ["1 week", 7.days],
+      ["2 weeks", 14.days],
+      ["4 weeks", 28.days],
+      ["2 months", 2.months],
+      ["3 months", 3.months],
+      ["6 months", 6.months],
+      ["1 year", 1.year]
     ]
   end
   
