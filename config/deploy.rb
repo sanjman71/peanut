1e6f26b229fe75b6b03a9734084fd0ede26f083c
Killian Murphy <killian@killianmurphy.com>
2009-10-13 14:30:13 -0700

    updates for deploying peanut to staging site

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,17 +12,33 @@ set :deploy_to,     "/usr/apps/#{application}"
 
 # Git repository
 set :scm,           :git
-set :repository,    'git@github.com:sanjman71/peanut.git'
+set :repository,    'git@peanut_github:sanjman71/peanut.git'
 set :branch,        "master"
 set :deploy_via,    :remote_cache
 
 # Users, groups
-set :user,          'peanut'  # log into servers as
-set :group,         'peanut'
+set :user,          'app'  # log into servers as
+set :group,         'app'
 
 # Load external recipe files
-load_paths << "config/mongrel"
-load_paths << "config/database"
-load "mongrel_tasks"
-load "database_tasks"
+load_paths << "config/recipes"
+load "crontab"
+load "database"
+load "delayed_job"
 
+deploy.task :restart, :roles => :app do
+  run "touch #{current_release}/tmp/restart.txt"
+end
+
+deploy.task :init, :roles => :app do
+  # first time initialization
+  run "mkdir -p #{deploy_to}/releases"
+  run "mkdir -p #{deploy_to}/shared/log"
+  run "mkdir -p #{deploy_to}/shared/pids"
+end
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
+
+after "deploy", "database:configure"
