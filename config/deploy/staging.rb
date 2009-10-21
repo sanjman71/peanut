e53386adeef0178e4fe1bcdf09542613bfba5d2a
Sanjay <sanjay@jarna.com>
2009-10-20 22:14:48 -0500

    changed deploy files for mossocalendar1

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 # Create hosts hash, especially useful when there is more than 1 host
-hosts               = Hash.new
-hosts[:cloud_servers]   = '174.143.204.171:30001'
+hosts                   = Hash.new
+hosts[:cloud_servers]   = '67.23.46.244:30001'
 
 # Set roles
 role :app,          hosts[:cloud_servers]
