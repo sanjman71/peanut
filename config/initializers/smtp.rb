61d4ce796171fc6c6a32120e2d92137b41ddbcf1
Sanjay <sanjay@jarna.com>
2009-12-09 18:40:13 -0600

    fixed smtp configuration

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,6 +3,6 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => "587",
   :authentication => :plain,
-  :user_name => "peanut@jarna.com",
-  :password => "peanut4all" 
+  :user_name => "app@walnutindustries.com",
+  :password => "1ndus7ry!"
 }
