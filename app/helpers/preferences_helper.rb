73f853659dbc8afc7a1202ac6d0ae92341425005
Sanjay <sanjay@jarna.com>
2009-12-15 15:11:00 -0800

    added company preference interface for appt start times

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -10,12 +10,18 @@ module PreferencesHelper
       ["1 year", 1.year]
     ]
   end
-  
+
   def start_wday_options
     [
       ["Sunday", "0"],
       ["Monday", "1"]
     ]
   end
-  
+
+  def appt_start_minutes_options
+    [
+      ["On the hour only", "[0]"],
+      ["Every 30 minutes", "[0,30]"]
+    ]
+  end
 end
